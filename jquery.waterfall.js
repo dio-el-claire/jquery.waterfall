3d2d493b3fa08e2f6bd7b4298bb3205e320b8b13
Dmitry (dio) Levashov <dio@std42.ru>
2011-06-16 15:58:33 +0400

    minor fixes. example updated

--- jquery.waterfall.js
+++ jquery.waterfall.js
@@ -2,14 +2,12 @@
 /**
  * Runs functions given in arguments in series, each functions passing their results to the next one.
  * Return jQuery Deferred object.
- * При успешном завершении всех фунции их результаты передаются в resolve обработчик Deferred object.
- * Если одна из функций завершится с ошибкой будет вызван reject обработчик Deferred объекта.
  *
  * @example
  * $.waterfall(
- *    function() { $.ajax({url : first_url}) },
- *    function() { $.ajax({url : second_url}) },
- *    function() { $.ajax({url : another_url}) }
+ *    function() { return $.ajax({url : first_url}) },
+ *    function() { return $.ajax({url : second_url}) },
+ *    function() { return $.ajax({url : another_url}) }
  *).fail(function() {
  *    console.log(arguments)
  *).done(function() {
@@ -30,15 +28,12 @@ $.waterfall = function() {
 
 			if (typeof(a) == 'function') {
 				if (!((d = a.apply(null, args)) && d.promise)) {
-					d = $.Deferred()[d === false ? 'reject' : 'resolve'](d)
+					d = $.Deferred()[d === false ? 'reject' : 'resolve'](d);
 				}
 			} else if (a && a.promise) {
 				d = a;
-				if (!d.isResolved() && !d.isRejected()) {
-					d.resolve.apply(d, args);
-				}
 			} else {
-				d = $.Deferred().resolve(a);
+				d = $.Deferred()[a === false ? 'reject' : 'resolve'](a);
 			}
 
 			d.fail(function() {
@@ -48,11 +43,9 @@ $.waterfall = function() {
 				pointer++;
 				args.push(data);
 
-				if (pointer == steps.length) {
-					dfrd.resolve.apply(dfrd, args);
-				} else {
-					steps[pointer].apply(null, args);
-				}
+				pointer == steps.length
+					? dfrd.resolve.apply(dfrd, args)
+					: steps[pointer].apply(null, args);
 			});
 		});
 	});
